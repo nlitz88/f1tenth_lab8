435ab4929e60f77bd3c06cce20ef7581845fd802
Nathan Litzinger <nlitz88@gmail.com>
2023-12-01 16:22:32 -0500

    Refactored detection script with a more simple approach outlined in the learnopencv tutorial

--- detection.py
+++ detection.py
@@ -10,6 +10,7 @@ import numpy as np
 import tensorrt as trt
 import pycuda.driver as cuda
 import pycuda.autoinit
+import torch
 
 # https://github.com/NVIDIA/TensorRT/blob/main/quickstart/IntroNotebooks/4.%20Using%20PyTorch%20through%20ONNX.ipynb
 # Slight more relevant object detection tutorial with YoloV3.
@@ -28,40 +29,22 @@ import pycuda.autoinit
 # 4. Create a "postprocessing" function where we draw the predicted bounding box
 #    onto the image (can use openCV to do this).
 
-# Taken from
-# https://github.com/NVIDIA/TensorRT/blob/release/8.4/samples/python/common.py#L123
-class HostDeviceMem(object):
-    def __init__(self, host_mem, device_mem):
-        self.host = host_mem
-        self.device = device_mem
-
-    def __str__(self):
-        return "Host:\n" + str(self.host) + "\nDevice:\n" + str(self.device)
-
-    def __repr__(self):
-        return self.__str__()
-
-# Taken from
-# https://github.com/NVIDIA/TensorRT/blob/release/8.4/samples/python/common.py#L136
+# Adapted from
+# https://learnopencv.com/how-to-convert-a-model-from-pytorch-to-tensorrt-and-speed-up-inference/
 def allocate_buffers(engine):
-    inputs = []
-    outputs = []
-    bindings = []
-    stream = cuda.Stream()
     for binding in engine:
-        size = trt.volume(engine.get_tensor_shape(binding)) * engine.max_batch_size
-        dtype = trt.nptype(engine.get_tensor_dtype(binding))
-        # Allocate host and device buffers
-        host_mem = cuda.pagelocked_empty(size, dtype)
-        device_mem = cuda.mem_alloc(host_mem.nbytes)
-        # Append the device buffer to device bindings.
-        bindings.append(int(device_mem))
-        # Append to the appropriate list.
-        if engine.get_tensor_mode(binding):
-            inputs.append(HostDeviceMem(host_mem, device_mem))
-        else:
-            outputs.append(HostDeviceMem(host_mem, device_mem))
-    return inputs, outputs, bindings, stream
+        if engine.binding_is_input(binding):  # we expect only one input
+            input_shape = engine.get_binding_shape(binding)
+            input_size = trt.volume(input_shape) * engine.max_batch_size * np.dtype(np.float32).itemsize  # in bytes
+            input_dtype = trt.nptype(engine.get_tensor_dtype(binding))
+            host_input = cuda.pagelocked_empty(input_size, input_dtype)
+            device_input = cuda.mem_alloc(input_size)
+        else:  # and one output
+            output_shape = engine.get_binding_shape(binding)
+            # create page-locked memory buffers (i.e. won't be swapped to disk)
+            host_output = cuda.pagelocked_empty(trt.volume(output_shape) * engine.max_batch_size, dtype=np.float32)
+            device_output = cuda.mem_alloc(host_output.nbytes)
+    return host_input, device_input, host_output, device_output
 
 def main():
 
@@ -94,16 +77,16 @@ def main():
     f = open(trt_model_path, "rb")
     runtime = trt.Runtime(trt.Logger(trt.Logger.WARNING)) 
 
-    engine = runtime.deserialize_cuda_engine(f.read())
-    context = engine.create_execution_context()
+    f1yolo_engine = runtime.deserialize_cuda_engine(f.read())
+    context = f1yolo_engine.create_execution_context()
 
     # Allocate memory for input and output.
-    inputs, outputs, bindings, stream = allocate_buffers(engine=engine)
-    # Inputs is like a pointer to a collection of GPU memory locations. Likewise
-    # for outputs. To run inference on our model and get the results, we
-    # essentially have to load our image data from system memory into one of
-    # those GPU memory input locations, then get the results in one of the
-    # output GPU memory locations.
+    host_input, device_input, host_output, device_output = allocate_buffers(engine=f1yolo_engine)
+    # host_input is a region of page_locked memory on the host, while
+    # device_input is a memory location allocated on the GPU. Likewise,
+    # host_output is a region of page_locked memory on the host, and
+    # device_output is the memory region on the device that is allocated to
+    # store the output of the engine.
 
     # Also, we create what's called a CUDA stream object. A CUDA stream is kind
     # of like an OS thread--it's an independent sequence of (CUDA) instructions.
@@ -112,15 +95,26 @@ def main():
     # streams to process multiple images asynchronously if you needed to. To add
     # instructions to this stream, we can specify this stream as an argument to
     # each of the CUDA operations that we specify below.
+    inference_stream = cuda.Stream()
 
     # Pass image through model.
     print(f"Running inference on {test_image_path}")
-    inputs[0].host = test_image
+    # Copy the input image into the host_input page_locked memory.
+    host_input = test_image
+    # Copy the image data in the hosts memory to the device's memory.
+    cuda.memcpy_htod_async(device_input, host_input, inference_stream)
+    # Run inference. This essentially tells teh CPU to take the input and pass
+    # it through the engine, place the result in the output location.
+    context.execute_async(bindings=[int(device_input), int(device_output)],
+                          stream_handle=inference_stream.handle)
+    # Copy the results from GPU memory back to the host.
+    cuda.memcpy_dtoh_async(host_output, device_output, inference_stream)
+    inference_stream.synchronize()
 
-    # Transfer input image from the "host to the device" (htod), where the
-    # device is the GPU.
-    
-
+    # Examine dimensions of output data, then we can figure out how to reshape
+    # it before post-processing.
+    result_tensor = torch.Tensor(host_output)
+    print(f"Result vector has shape: {result_tensor.shape}")
 
 if __name__ == "__main__":
 
