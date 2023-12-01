dfe9695ba581f0d2a4c18c371d87d7e071285a6c
Nathan Litzinger <nlitz88@gmail.com>
2023-12-01 03:26:13 -0500

    Added precision parameter to conversion function

--- convert_trt.py
+++ convert_trt.py
@@ -14,6 +14,7 @@ own.
 
 from pathlib import Path
 from typing import Optional, Tuple
+import subprocess
 
 import torch.onnx
 from torch import nn
@@ -40,6 +41,9 @@ def f1tenth_torch_to_onnx(model_weights_path: Path,
     Raises:
         FileNotFoundError: Thrown if the provided weights file doesn't exist.
         Exception: Thrown in the provided output directory doesn't exist.
+    
+    Returns:
+        str: The path of the resulting onnx file if successfully created.
     """
 
     # Check to see if the model exists at the provided path.
@@ -52,8 +56,10 @@ def f1tenth_torch_to_onnx(model_weights_path: Path,
     # model.
     # https://pytorch.org/tutorials/beginner/saving_loading_models.html#saving-loading-model-for-inference
     f1yolo_model = F110_YOLO()
+    print(f"Loading model weights.")
     state_dict = torch.load(f=model_weights_path)
     f1yolo_model.load_state_dict(state_dict)
+    print(f"Loaded model weights.")
 
     # Convert to onnx. 
     # https://docs.nvidia.com/deeplearning/tensorrt/quick-start-guide/index.html#export-from-pytorch
@@ -62,34 +68,59 @@ def f1tenth_torch_to_onnx(model_weights_path: Path,
     # TODO Need to add batch size as first dimension.
     # Note that input shape is (B, C, H, W)
     dummy_input = torch.zeros(size=(batch_size, *input_size))
-    print(f"Dummy input shape: {dummy_input.shape}")
-    
+
     # Also, create a new path within the provided output directory.
-    onnx_model_path = output_directory/r"f1yolo.onnx"
+    onnx_model_path = output_directory/f"{Path(model_weights_path.parts[-1]).stem}.onnx"
+    print(f"Onnx model path: {onnx_model_path}")
 
     # https://pytorch.org/docs/stable/onnx_torchscript.html#torch.onnx.export
+    print(f"Beginning onnx conversion.")
     torch.onnx.export(model=f1yolo_model,
                       args=dummy_input,
                       f=onnx_model_path,
-                      verbose=True)
+                      verbose=False)
+    print(f"Finished onnx conversion.")
     
-    return
+    return onnx_model_path
 
 # Note that this operation can very easily be carried out via the trtexec
-# command line utility provided as a part of the TensorRT SDK. This function is
-# an example of converting an ONNX model to a TensorRT engine using the TensorRT
-# python api/library.
+# command line utility provided as a part of the TensorRT SDK. For the sake of
+# simplicity, I'm going to use the trtexec command line utility. Was originally
+# going to use the Python API to do the conversion, but there doesn't appear to
+# be a ton of documentation for it.
 
 # Following this guide:
 # https://docs.nvidia.com/deeplearning/tensorrt/quick-start-guide/index.html#convert-onnx-engine
-def onnx_to_tensorrt() -> None:
+def onnx_to_tensorrt(onnx_model_path: Path,
+                     quantization: str) -> None:
+    """Converts the provided onnx model to a TensorRT engine.
 
+    Args:
+        onnx_model_path (Path): Filepath of the onnx model to convert
+        quantization (str): Precision that the resulting engine/model should
+        have. Choose from ["fp32", "fp16", "fp8", "int8"].
 
-    pass
+    Raises:
+        Exception: Throws exception if an invalid precision/quantization
+        provided.
+    """
+    # https://docs.nvidia.com/deeplearning/tensorrt/developer-guide/index.html#trtexec-flags
+
+    if quantization == "fp32":
+        subprocess.run(args=["trtexec", f"--onnx={onnx_model_path}", f"--saveEngine={Path(onnx_model_path.parts[-1]).stem}_{quantization}.trt"])
+    elif quantization == "fp16" or quantization == "int8" or quantization == "fp8":
+        subprocess.run(args=["trtexec", f"--onnx={onnx_model_path}", f"--{quantization}", f"--saveEngine={Path(onnx_model_path.parts[-1]).stem}_{quantization}.trt"])
+    else:
+        raise Exception(f"Invalid/unsupported quantization/precision level {quantization} provided. Please choose from fp32, fp16, fp8, int8")
+    return
 
 if __name__ == "__main__":
     
-    # 1. Convert model to ONNX.
+    # 1. Convert model to onnx format.
     model_weights_path = Path(r"./f1yolo.pt")
-    f1tenth_torch_to_onnx(model_weights_path=model_weights_path,
-                          input_size=(3, 180, 320))
+    onnx_model_path = f1tenth_torch_to_onnx(model_weights_path=model_weights_path,
+                                            input_size=(3, 180, 320))
+    
+    # 2. Convert the onnx model to a TensorRT Engine.
+    onnx_to_tensorrt(onnx_model_path=onnx_model_path,
+                     quantization="int8")
--- detection.py
+++ detection.py
@@ -2,6 +2,8 @@
 inference on our F1Tenth Car Detection (Object Detection) model.
 """
 
+import time
+
 # 1. Create a function to load the TensorRT model.
 
 # 2. Create a function where we invoke the TensorRT "engine" (model). Will
@@ -12,3 +14,7 @@ inference on our F1Tenth Car Detection (Object Detection) model.
 
 # 4. Create a "postprocessing" function where we draw the predicted bounding box
 #    onto the image (can use openCV to do this).
+
+
+# https://github.com/NVIDIA/TensorRT/blob/main/quickstart/IntroNotebooks/4.%20Using%20PyTorch%20through%20ONNX.ipynb
+
