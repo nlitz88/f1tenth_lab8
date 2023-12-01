c828edfc6432be0f3c22dc2ff3ac25e7522e7b90
Nathan Litzinger <nlitz88@gmail.com>
2023-12-01 01:32:28 -0500

    Outlined plan for each module

--- convert_trt.py
+++ convert_trt.py
@@ -2,3 +2,12 @@
 models.
 """ 
 
+from pathlib import Path
+
+import torch.onnx
+
+def torch_to_onnx(torch_model_path: Path,
+                  output_directory: Path) -> None:
+    
+    # Attempt to load model.
+    pass
--- detection.py
+++ detection.py
@@ -2,3 +2,12 @@
 inference on our F1Tenth Car Detection (Object Detection) model.
 """
 
+# 1. Create a function to load the TensorRT model.
+
+# 2. Create a function where we invoke the TensorRT "engine" (model). Will
+#    likely have to use the TensorRT python package.
+
+# 3. Create "preprocessing" function. I.e., can resize images if need be.
+
+# 4. Create a "postprocessing" function where we draw the predicted bounding box
+#    onto the image (can use openCV to do this).
